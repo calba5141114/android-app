51369f281b509861d63beb922e5c16fc19e9d6d9
Carlos Alba <calba5141114@Carloss-MacBook-Pro.local>
2018-09-23 15:27:22 -0700

    calculate income

--- app/src/main/java/com/example/calba5141114/myapplication/MainActivity.java
+++ app/src/main/java/com/example/calba5141114/myapplication/MainActivity.java
@@ -19,34 +19,65 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    // This is the event triggered when the user clicks the submit button
+    public void eventOneClick(View view) {
 
-    public void eventOneClick(View view){
-
-        Intent intent = new Intent(this,Main2Activity.class);
+        Intent intent = new Intent(this, Main2Activity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
 
         // setting the value of the message string = to the value of the dit text element in XML mainactivity
         String message = editText.getText().toString();
 
-        if (message.length() <= 0){
-
+        if (message.length() <= 0) {
             Context context = getApplicationContext();
-            CharSequence text =  " Your message needs to be longer than 0 characters!";
+            CharSequence text = " Your message needs to be longer than 0 characters!";
             int duration = Toast.LENGTH_SHORT;
-            Toast toast  =  Toast.makeText(context, text, duration);
+            Toast toast = Toast.makeText(context, text, duration);
             toast.show();
-
-        }
-        else{
-
-            intent.putExtra(EXTRA_MESSAGE,message);
+        } else {
+            intent.putExtra(EXTRA_MESSAGE, message);
             startActivity(intent);
-
         }
-
 
     }
 
+    public double calculateIncome(double money, double[] totalMoney) {
+
+        double endOfArrIndex = totalMoney.length + 1;
+        totalMoney[(int) endOfArrIndex] = money;
+        double total = 0;
+
+        for (int i = 0; i <= totalMoney.length; i++) {
+            double v = totalMoney[i];
+            total += v;
+        }
+
+        return total;
+
+    }
+
+    public void calculateIncomeEvent(View view) {
+
+        double money[] = new double[0];
+
+        EditText editTextCalc = (EditText) findViewById(R.id.editText2);
+
+        String stringAmount = editTextCalc.getText().toString();
+        
+        Double amount = Double.parseDouble(stringAmount);
+
+        Double total = calculateIncome(amount, money);
+
+        String totalFinalized = total.toString();
+
+        Context context = getApplicationContext();
+
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, totalFinalized, duration);
+        toast.show();
+
+    }
 
 
 }
